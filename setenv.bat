ef999f8632e268416745b2fcc1b46dc8b65c64ea
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-08 11:12:22 +0900

    Changed make rules.

--- setenv.bat
+++ setenv.bat
@@ -2,4 +2,4 @@
 set GCC_HOME=c:\MinGW
 set LANG=C
 set SWO_HOME=/home/swo
-set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%GCC_HOME%\bin
+set PATH=%GCC_HOME%\bin;%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
