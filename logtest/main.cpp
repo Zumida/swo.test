6391440c6d9acc38cc92a233696bd941bf62cbec
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-18 02:20:31 +0900

    Add logging test.

---
+++ logtest/main.cpp
@@ -0,0 +1,47 @@
+#include "swo.hpp"
+#include "logger.hpp"
+#include <exception>
+
+using namespace swo;
+
+class MyException : public std::exception {
+private:
+	std::string message;
+public:
+	MyException(const std::string& message) : message(message) {}
+	const char* what() const throw() {return message.c_str();}
+};
+
+MakeApplication(
+class MyRunner : public Runner {
+
+public:
+	void run(void) {
+		/*
+		 * 標準出力/標準エラー出力/標準ログ出力は、
+		 * DebugViewまたはgdbにて確認すること！
+		 */
+		LOG_I(L"通常ログ出力");
+
+		LOG_W(L"警告ログ出力");
+
+		LOG_E(L"エラーログ出力");
+
+		LOG_D(L"デバッグログ出力");
+
+		try {
+			throw std::exception();
+
+		} catch (std::exception &e) {
+			LOG_EX(e);
+		}
+
+		try {
+			throw MyException("Exception log!");
+
+		} catch (std::exception &e) {
+			LOG_EX(e);
+		}
+	}
+
+});
