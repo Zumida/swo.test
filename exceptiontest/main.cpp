3aa0b385778c05df8902829841f59c471cc9bca3
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-25 14:14:56 +0900

    Add exception test.

--- exceptiontest/main.cpp
+++ exceptiontest/main.cpp
@@ -1,12 +1,38 @@
 #include "swo.hpp"
 #include "form.hpp"
-#include <exception>
+#include "exception.hpp"
 
 using namespace swo;
 
 MakeApplication(
 class MyRunner : public Runner {
 	void run(void) {
+
+		try {
+			throw Exception(L"Test1");
+
+		} catch (const Exception& e) {
+			Stdlog << L"e.getMessage()=" << e.getMessage() << std::endl;
+			Stdlog << L"e.getCause()=" << e.getCause() << std::endl;
+			Stdlog << L"e.what()=" << e.what() << std::endl;
+			Stdlog << L"e.getDetail()=" << e.getDetail() << std::endl;
+		}
+
+		try {
+			try {
+				throw Exception(L"Test1");
+
+			} catch (const Exception& e) {
+				throw Exception(L"Test2", &e);
+			}
+
+		} catch (const Exception& e) {
+			Stdlog << L"e.getMessage()=" << e.getMessage() << std::endl;
+			Stdlog << L"e.getCause()=" << e.getCause() << std::endl;
+			Stdlog << L"e.what()=" << e.what() << std::endl;
+			Stdlog << L"e.getCause()->getMessage()=" << e.getCause()->getMessage() << std::endl;
+			Stdlog << L"e.getDetail()=" << e.getDetail() << std::endl;
+		}
 
 		Form& form = Form::create();
 		form.show();
