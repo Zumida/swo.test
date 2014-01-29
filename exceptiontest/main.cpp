e742ef36221e365e8d1b40b90046991b9a79d9ba
MIRROR_ <f1999sun4ssm@yahoo.co.jp>
2014-01-29 18:37:38 +0900

    Add exception test.
    Add nested exception test.
    Add my exception class test.

--- exceptiontest/main.cpp
+++ exceptiontest/main.cpp
@@ -4,6 +4,13 @@
 
 using namespace swo;
 
+class MyException : public Exception {
+public:
+	MyException(const String& message) : Exception(message) {}
+	MyException(const String& message, const Exception& cause)
+	: Exception(message,cause) {}
+};
+
 MakeApplication(
 class MyRunner : public Runner {
 	void run(void) {
@@ -23,7 +30,39 @@ class MyRunner : public Runner {
 				throw Exception(L"Test1");
 
 			} catch (const Exception& e) {
-				throw Exception(L"Test2", &e);
+				throw Exception(L"Test2", e);
+			}
+
+		} catch (const Exception& e) {
+			Stdlog << L"e.getMessage()=" << e.getMessage() << std::endl;
+			Stdlog << L"e.getCause()=" << e.getCause() << std::endl;
+			Stdlog << L"e.what()=" << e.what() << std::endl;
+			Stdlog << L"e.getCause()->getMessage()=" << e.getCause()->getMessage() << std::endl;
+			Stdlog << L"e.getDetail()=" << e.getDetail() << std::endl;
+		}
+
+		try {
+			try {
+				throw MyException(L"Test3");
+
+			} catch (const Exception& e) {
+				throw Exception(L"Test4", e);
+			}
+
+		} catch (const Exception& e) {
+			Stdlog << L"e.getMessage()=" << e.getMessage() << std::endl;
+			Stdlog << L"e.getCause()=" << e.getCause() << std::endl;
+			Stdlog << L"e.what()=" << e.what() << std::endl;
+			Stdlog << L"e.getCause()->getMessage()=" << e.getCause()->getMessage() << std::endl;
+			Stdlog << L"e.getDetail()=" << e.getDetail() << std::endl;
+		}
+
+		try {
+			try {
+				throw Exception(L"Test5");
+
+			} catch (const Exception& e) {
+				throw MyException(L"Test6", e);
 			}
 
 		} catch (const Exception& e) {
